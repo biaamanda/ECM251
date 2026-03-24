5e403b1093a48f768f624fd63cf35c25b949f92d
Amanda Bialer <amandabialer@gmail.com>
2026-03-24 07:53:45 -0300

    add aula 6

---
+++ aula 6/ex01turma.java
@@ -0,0 +1,10 @@
+/*Crie a classe Turma com seu construtor, métodos de acesso e
+modificadores e os atributos privados nome, do tipo String,
+curso, do tipo String, quantidadeDeAlunos, do tipo int, serie,
+do tipo int */
+
+public class ex01turma {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ aula 6/ex02testeTurma.java
@@ -0,0 +1,12 @@
+/*Crie a classe TesteTurma com o método main. De modo
+análogo ao exemplo, peça para o usuário entrar com os
+valores necessários para criar uma turma, instancie um objeto
+Turma e depois exiba os dados da turma criada. Depois, peça
+para o usuário uma nova quantidade de alunos, altere o valor
+do atributo e exiba os dados novamente; */
+
+public class ex02testeTurma {
+    public static void main(String[] args) {
+        
+    }
+}

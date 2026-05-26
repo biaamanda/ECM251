016bd28e408a243a31a3ea0282c1e12bc8eb5baf
Amanda Bialer <amandabialer@gmail.com>
2026-05-26 08:05:50 -0300

    add aula 12

---
+++ aula 12/ex01.java
@@ -0,0 +1,47 @@
+/*1. Utilizando-se de JMenu e JMenuItem, desenvolver um
+aplicativo em Java capaz de atender às seguintes solicitações:
+a. Criar um menu denominado Vetor. Ao ser selecionado,
+apresente os itens denominados Dimensiona, Digita e Apresenta.
+• No item Dimensiona será solicitado o tamanho de um vetor do tipo double;
+• No item Digita serão solicitados todos os elementos deste vetor;
+• No item Apresenta serão apresentados todos os elementos deste vetor.
+Utilizando-se de JMenu e JMenuItem, desenvolver um aplicativo
+em Java capaz de atender às seguintes solicitações:
+b. Criar um menu denominado PROBEST. Ao ser selecionado,
+apresente os itens denominados Média, Desvio Padrão,
+Variância, Mediana, Coef. Assimetria e Coef. Variação.
+• Ao ser selecionado cada um destes itens, deverá ser
+apresentado o seu respectivo cálculo estatístico, sobre o vetor
+digitado no menu Vetor;
+Utilizando-se de JMenu e JMenuItem, desenvolver um aplicativo
+em Java capaz de atender às seguintes solicitações:
+c. Criar um menu denominado CDI. Ao ser selecionado,
+apresente os itens denominados Função e Derivada.
+• Ao ser selecionado cada um destes itens, deverá ser
+apresentado o seu respectivo cálculo, sobre o vetor digitado
+no menu Vetor;
+Utilizando-se de JMenu e JMenuItem, desenvolver um aplicativo
+em Java capaz de atender às seguintes solicitações:
+d. Criar um menu denominado ALGESD. Ao ser selecionado,
+apresente os itens denominados Ordenação e Busca.
+• Caso seja selecionado o item Ordenação, deverão ser
+apresentados os itens Trocas, Seleção, Inserção, Quick e
+Merge.
+• Ao ser selecionado cada um destes itens, deverá ser realizada
+a ordenação do vetor digitado no menu Vetor, através do
+respectivo algoritmo de ordenação escolhido.
+Utilizando-se de JMenu e JMenuItem, desenvolver um aplicativo
+em Java capaz de atender às seguintes solicitações:
+• Caso seja selecionado o item Busca, deverão ser apresentados
+os itens Linear Iterativa, Linear Recursiva, Binária Iterativa e
+Binária Recursiva;
+• Ao ser selecionado cada um destes itens, deverá ser solicitado
+um valor chave para a busca e, a seguir, realizada a busca
+desse valor chave no vetor digitado no menu Vetor, através
+do respectivo algoritmo de busca;
+Utilizando-se de JMenu e JMenuItem, desenvolver um aplicativo
+em Java capaz de atender às seguintes solicitações:
+• Lembrar que para realizar qualquer uma das buscas binárias
+oferecidas, o vetor deverá estar ordenado antecipadamente
+(solicitar a escolha do algoritmo para realizar a prévia
+ordenação). */

3a7018099828a78c066225ec85d70432d4aeb2ac
Amanda Bialer <amandabialer@gmail.com>
2026-08-25 08:14:03 -0300

    add aula 15

---
+++ aula 15/Ex01.java
@@ -0,0 +1,12 @@
+/*Reescreva as classes TelaExemplo e TelaExemploTest,
+fornecidas a seguir, adaptando a interface com o usuário do
+idioma Inglês para os idiomas Português, Inglês, Francês e
+Italiano. Para a escolha do idioma, insira essa opção de
+escolha no Menu, através de itens, no próprio contexto do
+aplicativo; e */
+
+//package aula 15;
+
+public class Ex01 {
+    
+}
---
+++ aula 15/Ex02.java
@@ -0,0 +1,11 @@
+/* Internacionalizar o exercício proposto 2 (dois) da aula 14
+(pg. 39 do material “IMT-2025-ECM251-Lab-Aula14-
+ManipulaçãoDeÍconesSenhas&Datas-ProfCalvetti.pdf”) para
+os idiomas Português e Inglês, escolhidos através de um
+menu, antes do usuário realizar o seu “login”. */
+
+//package aula 15;
+
+public class Ex02 {
+    
+}
